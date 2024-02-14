43f564a6025e9ad19634a7e2ea90f159baa2e417
Qaisar <kayser.abbas@gmail.com>
2024-02-14 16:44:14 +0500

    Update home page with new picture

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,8 +1,7 @@
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
-import homeLogo from "../../Assets/home-main.svg";
+import developer from "../../Assets/developer3.png";
 import Particle from "../Particle";
-import Home2 from "./Home2";
 import Type from "./Type";
 
 function Home() {
@@ -13,18 +12,16 @@ function Home() {
         <Container className="home-content">
           <Row>
             <Col md={7} className="home-header">
-              <h1 style={{ paddingBottom: 15 }} className="heading">
-                Full Stack Developer | Code Reviewer | Team Player | Remote Cross-cultural Collaboration
-!{" "}
-                <span className="wave" role="img" aria-labelledby="wave">
-                  👋🏻
-                </span>
-              </h1>
-
               <h1 className="heading-name">
-                I'M
-                <strong className="main-name"> QAISAR ABBAS</strong>
+                I'm
+                <strong className="main-name"> Qaisar </strong>
               </h1>
+              <h2 style={{ paddingBottom: 15 }} className="heading">
+                Software Developer | Code Reviewer | Team Player | Remote Cross-cultural Collaboration{" "}
+              </h2>
+              <p style={{ paddingBottom: 5 }} className="paragraph">
+                 I can help you build a product, a feature or a website. Look through some of my work and experience. If you like what you see and have a project you need coded, don't hestiate to contact me.
+              </p>
 
               <div style={{ padding: 50, textAlign: "left" }}>
                 <Type />
@@ -33,16 +30,14 @@ function Home() {
 
             <Col md={5} style={{ paddingBottom: 20 }}>
               <img
-                src={homeLogo}
+                src={developer}
                 alt="home pic"
-                className="img-fluid"
-                style={{ maxHeight: "450px" }}
+                className="developer"
               />
             </Col>
           </Row>
         </Container>
       </Container>
-      <Home2 />
     </section>
   );
 }
