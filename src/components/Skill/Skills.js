b1d2830a31da996e732c1e141d1bfb1faca378a2
Qaisar <kayser.abbas@gmail.com>
2024-02-15 18:35:40 +0500

    Remove unused Row, Col

--- src/components/Skill/Skills.js
+++ src/components/Skill/Skills.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Container, Row, Col } from 'react-bootstrap';
+import { Container } from 'react-bootstrap';
 import Particle from '../Particle';
 import Github from './Github';
 import Techstack from './Techstack';
