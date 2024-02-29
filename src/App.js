377a253cfd3d789febe5ac78c650ba0ddc12a372
Kaiserabbas <123939543+Kaiserabbas@users.noreply.github.com>
2024-02-29 11:58:21 +0500

    Add credentials component

--- src/App.js
+++ src/App.js
@@ -13,6 +13,7 @@ import Skill from './components/Skill/Skills';
 import Projects from './components/Projects/Projects';
 import Footer from './components/Footer';
 import Resume from './components/Resume/ResumeNew';
+import Credentials from './components/Credentials/Credentials';
 import ScrollToTop from './components/ScrollToTop';
 import './style.css';
 import './App.css';
@@ -41,6 +42,7 @@ function App() {
           <Route path="/skill" element={<Skill />} />
           <Route path="/about" element={<About />} />
           <Route path="/resume" element={<Resume />} />
+          <Route path="/credentials" element={<Credentials />} />
           <Route path="*" element={<Navigate to="/" />} />
         </Routes>
         <Footer />
